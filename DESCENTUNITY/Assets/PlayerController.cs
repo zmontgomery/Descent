ca5b2481a65abeee633f41800c0abe1e97a61880
Taeshinobu <kobe.laprade@gmail.com>
2020-10-12 13:55:40 -0400

    bitcuh

---
+++ DESCENTUNITY/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    private float vert;
+    private float horiz;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        vert = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        horiz = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(horiz, 0, vert);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ DESCENTUNITY/Assets/PlayerLook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    private float MouseX;
+    private float MouseY;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        MouseX += Input.GetAxis("Mouse X");
+        MouseY += Input.GetAxis("Mouse Y");
+
+        transform.localRotation = Quaternion.Euler(-MouseY, MouseX, 0);
+
+    }
+
+}
