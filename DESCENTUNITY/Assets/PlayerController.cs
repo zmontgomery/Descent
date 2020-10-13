e6f30162a10bc39d2b9f7e6681976e05cc86f5f0
Taeshinobu <kobe.laprade@gmail.com>
2020-10-12 20:48:23 -0400

    67

--- DESCENTUNITY/Assets/PlayerController.cs
+++ DESCENTUNITY/Assets/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -20,18 +22,28 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        UnityEngine.Debug.Log(Physics.OverlapBox(transform.position - new Vector3(0, 1, 0), new Vector3(0.5f, 0.2f, 0.5f), Quaternion.identity, 0));
+
         vert = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         horiz = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         transform.Translate(horiz, 0, vert);
 
         if (Input.GetKeyDown("space"))
         {
-            rb.AddForce(0, 800, 0);
+            if (Physics.OverlapBox(transform.position - new Vector3(0, 1, 0), new Vector3(0.5f, 0.2f, 0.5f), Quaternion.identity, 0) != null)
+            {
+                rb.AddForce(0, 100, 0);  
+            }
         }
 
         if (Input.GetKeyDown("escape"))
         {
             Cursor.lockState = CursorLockMode.None;
         }
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(1, 1, 1));
     }
 }
