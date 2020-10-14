e3f28501dec503a4b21379b36db8f08454073a62
Zachary Montgomery <zsm5532@rit.edu>
2020-10-14 15:39:58 -0400

    Movement API refactor

---
+++ DESCENTUNITY/Assets/MovementAPI.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MovementAPI : MonoBehaviour
+{
+
+    public static float playerspeed = 3.0f;
+    public static float accel = 4f;
+    public static float airAccel = 0.2f;
+    public static float friction = 4f;
+    public static float airFriction = 0.2f;
+    public static float gravity = -9.81f;
+    public static float jumpHeight = 4.0f;
+    private static bool playerGrounded = false;
+    private static float vert;
+    private static float horiz;
+    private static float ykeyboard;
+    private static float xkeyboard;
+    private static float Dspeed = 0.0f;
+    private static float Aspeed = 0.0f;
+    private static float Wspeed = 0.0f;
+    private static float Sspeed = 0.0f;
+    private static Vector3 playerVel;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    static float acceleratePOS(float dirspeed, float accel){
+        if (dirspeed < playerspeed){
+            dirspeed += accel;
+            if(dirspeed > playerspeed){
+                dirspeed = playerspeed;
+            }
+        }
+        return dirspeed;
+    }
+
+    static float accelerateNEG(float dirspeed, float accel){
+        if (dirspeed > -playerspeed){
+            dirspeed -= accel;
+            if(dirspeed < -playerspeed){
+                dirspeed = -playerspeed;
+            }
+        }
+        return dirspeed;
+    }
+
+    static float decceleratePOS(float dirspeed, float friction){
+        dirspeed -= friction;
+        if(dirspeed < 0){
+            dirspeed = 0;
+        }
+        return dirspeed;
+    }
+
+    static float deccelerateNEG(float dirspeed, float friction){
+        dirspeed += friction;
+        if(dirspeed > 0){
+            dirspeed = 0;
+        }
+        return dirspeed;
+    }
+
+    static void changeHorizontalVel(bool status){
+        if (status == false){
+            if(xkeyboard > 0.0f){
+            Dspeed = acceleratePOS(Dspeed, airAccel);
+            }
+            else if(Dspeed > 0){
+                Dspeed = decceleratePOS(Dspeed, airFriction);
+            }
+            if(xkeyboard < 0.0f){
+                Aspeed = accelerateNEG(Aspeed, airAccel);
+            }
+            else if(Aspeed < 0){
+                Aspeed = deccelerateNEG(Aspeed, airFriction);
+            }
+            if(ykeyboard > 0.0f){
+                Wspeed = acceleratePOS(Wspeed, airAccel);
+            }
+            else if(Wspeed > 0){
+                Wspeed = decceleratePOS(Wspeed, airFriction);
+            }
+            if(ykeyboard < 0.0f){
+                Sspeed = accelerateNEG(Sspeed, airAccel);
+            }
+            else if(Sspeed < 0){
+                Sspeed = deccelerateNEG(Sspeed, airFriction);
+            }
+        }
+        if (status == true){
+            if(xkeyboard > 0.0f){
+                Dspeed = acceleratePOS(Dspeed, accel);
+            }
+            else if(Dspeed > 0){
+                Dspeed = decceleratePOS(Dspeed, friction);
+            }
+            if(xkeyboard < 0.0f){
+                Aspeed = accelerateNEG(Aspeed, accel);
+            }
+            else if(Aspeed < 0){
+                Aspeed = deccelerateNEG(Aspeed, friction);
+            }
+            if(ykeyboard > 0.0f){
+                Wspeed = acceleratePOS(Wspeed, accel);
+            }
+            else if(Wspeed > 0){
+                Wspeed = decceleratePOS(Wspeed, friction);
+            }
+            if(ykeyboard < 0.0f){
+                Sspeed = accelerateNEG(Sspeed, accel);
+            }
+            else if(Sspeed < 0){
+                Sspeed = deccelerateNEG(Sspeed, friction);
+            }
+        }
+    }
+
+
+    public static Vector3 GetMovements(bool playerGrounded, string preset = "")
+    {
+        if(preset == "Overwatch"){
+            playerspeed = 13.0f;
+            accel = 10f;
+            airAccel = 0.1f;
+            friction = 10f;
+            airFriction = 0.07f;
+            gravity = -10f;
+            jumpHeight = 3.6f;
+        }
+
+        ykeyboard = Input.GetAxis("Vertical");
+        xkeyboard = Input.GetAxis("Horizontal");
+
+        //ill explain later
+        changeHorizontalVel(playerGrounded);
+
+        //ADDS HORIZONTAL COMPONENTS TO MOVE VECTOR
+        horiz = Dspeed + Aspeed;
+        vert = Wspeed + Sspeed;
+        playerVel.x = horiz;
+        playerVel.z = vert;
+
+
+        //JUMP FUNCTION
+        if (playerGrounded && Input.GetKeyDown("space"))
+        {
+            playerVel.y += jumpHeight;
+        }
+
+        //MAKES SURE PLAYER STAYS GROUNDED WHEN GROUNDED
+        if (playerGrounded && playerVel.y < 0){
+            playerVel.y = 0f;
+        }
+        playerVel.y += gravity * Time.deltaTime;
+
+        return playerVel;
+    }
+
+}
--- DESCENTUNITY/Assets/PlayerController.cs
+++ DESCENTUNITY/Assets/PlayerController.cs
@@ -1,29 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float speed = 3.0f;
-    public float accel = 0.4f;
-    public float deccel = 0.4f;
-    public float gravity = -9.81f;
-    public float JumpHeight = 4.0f;
-    private bool playerGrounded = false;
-    private float vert;
-    private float horiz;
-    private float ykeyboard;
-    private float xkeyboard;
-    private float Dspeed = 0.0f;
-    private float Aspeed = 0.0f;
-    private float Wspeed = 0.0f;
-    private float Sspeed = 0.0f;
-    private Vector3 playerGrav;
+    public string preset = "Overwatch";
     private Vector3 playerVel;
+    private bool playerGrounded = false;
     CapsuleCollider capc;
     CharacterController cc;
 
@@ -31,105 +14,21 @@ public class PlayerController : MonoBehaviour
     {
         cc = GetComponent<CharacterController>();
         capc = GetComponent<CapsuleCollider>();
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
         playerGrounded = cc.isGrounded;
 
-        ykeyboard = Input.GetAxis("Vertical");
-        xkeyboard = Input.GetAxis("Horizontal");
-        if (playerGrounded){
-            if(xkeyboard > 0.0f){
-                if (Dspeed < speed){
-                    Dspeed += accel;
-                    if(Dspeed > speed){
-                        Dspeed = speed;
-                    }
-                }
-            }
-            else if(Dspeed > 0){
-                Dspeed -= deccel;
-                if(Dspeed < 0){
-                    Dspeed = 0;
-                }
-            }
-            if(xkeyboard < 0.0f){
-                if (Aspeed > -speed){
-                    Aspeed -= accel;
-                    if(Aspeed < -speed){
-                        Aspeed = -speed;
-                    }
-                }
-            }
-            else if(Aspeed < 0){
-                Aspeed += deccel;
-                if(Aspeed > 0){
-                    Aspeed = 0;
-                }
-            }
-            if(ykeyboard > 0.0f){
-                if (Wspeed < speed){
-                    Wspeed += accel;
-                    if(Wspeed > speed){
-                        Wspeed = speed;
-                    }
-                }
-            }
-            else if(Wspeed > 0){
-                Wspeed -= deccel;
-                if(Wspeed < 0){
-                    Wspeed = 0;
-                }
-            }
-            if(ykeyboard < 0.0f){
-                if (Sspeed > -speed){
-                    Sspeed -= accel;
-                    if(Sspeed < -speed){
-                        Sspeed = -speed;
-                    }
-                }
-            }
-            else if(Sspeed < 0){
-                Sspeed += deccel;
-                if(Sspeed > 0){
-                    Sspeed = 0;
-                }
-            }
-        }
-        
+        playerVel = MovementAPI.GetMovements(playerGrounded, preset);
 
-        horiz = Dspeed + Aspeed;
-        vert = Wspeed + Sspeed;
+        //MOVES PLAYER EACH GIVEN (playerVel) DIRECTION AT CURRENT FRAME
+        cc.Move(playerVel * Time.deltaTime * 0.5f);
 
-
-
-
-        playerVel = new Vector3(horiz, 0, vert);
-
-
-        
-
-
-        //playerVel.x += timeHeldW * Time.deltaTime;
-        //playerVel.z += timeHeldA * Time.deltaTime;
-        cc.Move(playerVel * Time.deltaTime * 2);
-
-
-        
-
-        if (playerGrounded && playerGrav.y < 0){
-            playerGrav.y = 0f;
-        }
-
-        playerGrav.y += gravity * Time.deltaTime;
-        cc.Move(playerGrav * Time.deltaTime);
-
+        // UNLOCKS CURSOR
         if (Input.GetKeyDown("escape"))
         {
             Cursor.lockState = CursorLockMode.None;
         }
     }
-
 }
