18e47453d64744b3390c4c0a66f63e8ad0a4701b
BuildTools <zsm5532@rit.edu>
2021-01-27 17:57:48 -0500

    scraps

---
+++ CapsuleLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CapsuleLook : MonoBehaviour
+{
+    private float MouseX;
+
+    void Start()
+    {
+        transform.localRotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    void Update()
+    {
+
+        MouseX += Input.GetAxis("Mouse X");
+
+        transform.localRotation = Quaternion.Euler(0, MouseX, 0);
+
+    }
+
+}
---
+++ PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public string preset = "";
+    private Vector3 playerVel;
+    private bool playerGrounded = false;
+    CapsuleCollider capc;
+    CharacterController cc;
+
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        capc = GetComponent<CapsuleCollider>();
+    }
+
+    void Update()
+    {
+        playerGrounded = cc.isGrounded;
+
+        playerVel = MovementAPI.GetMovements(playerGrounded, preset);
+
+        //MOVES PLAYER EACH GIVEN (playerVel) DIRECTION AT CURRENT FRAME
+        cc.Move(playerVel * Time.deltaTime * 0.5f);
+
+        // UNLOCKS CURSOR
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
