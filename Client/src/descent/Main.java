e9ce6c03d3cb6818682df8284782743053fe8db2
kobster68 <41244097+kobster68@users.noreply.github.com>
2022-07-27 15:12:57 -0400

    eeyyyy

--- Client/src/descent/Main.java
+++ Client/src/descent/Main.java
@@ -1,5 +1,14 @@
 package descent;
 
-public class Main {
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
+public class Main {
+	public static void main(String[] args) throws Exception {
+		DatagramSocket socket = new DatagramSocket();
+		DatagramPacket packet = new DatagramPacket(new byte[]{1}, 1, InetAddress.getByAddress(new byte[] { 66,66,83,160 }), 25565);
+		socket.send(packet);
+	}
 }
